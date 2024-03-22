67defc7e88bd94e5b3bdb4494a60bd7416ff90f3
TheDeaderzOne <137246950+TheDeaderzOne@users.noreply.github.com>
2024-03-22 19:33:37 -0400

    Add files via upload

---
+++ DeletingDivisors (1537D, 1700 CF).py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+testcases = int(input().replace("\n",""))
+
+arrgy = []
+
+for x in range(testcases):
+    arrgy.append(int(input().replace("\n","")))
+
+for x in range(len(arrgy)):
+    if arrgy[x]&1 == 1:
+        output("Bob"+"\n")
+    else:
+        if (arrgy[x]&(-1*arrgy[x]))==arrgy[x]:
+            counts = 0
+            tempholder = int(arrgy[x])
+            while (tempholder>>1) > 0:
+                counts+=1
+                tempholder>>=1
+            
+            if counts & 1 == 0:
+                output("Alice"+"\n")
+            else:
+                output("Bob"+"\n")
+
+        else:
+            output("Alice"+"\n")
+
---
+++ MedianString (1144E, 1900 CF).py
@@ -0,0 +1,66 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+StringLength = int(input().replace("\n",""))
+
+FirstWordList = [ord(char)-96 for char in list(input().replace("\n",""))]
+
+SecondWordList = [ord(char)-96 for char in list(input().replace("\n",""))]
+
+Taker = []
+
+def BackWardsWorker(takerlist,firstwordarr,secondwordarr,stringlen):
+    commonpointer = stringlen-1
+    if int((firstwordarr[commonpointer]+secondwordarr[commonpointer])/2)>=14:
+        takerlist.append(1)
+    else:
+        takerlist.append(0)
+    commonpointer-=1
+    while commonpointer > -1:
+        if (firstwordarr[commonpointer]+secondwordarr[commonpointer])&1 == 1:
+            if takerlist[-1]==0:
+                if int((firstwordarr[commonpointer]+secondwordarr[commonpointer])//2)>=14:
+                    takerlist.append(1)
+                else:
+                    takerlist.append(0)
+            else:
+                if int(((firstwordarr[commonpointer]+secondwordarr[commonpointer])//2)+1)>=14:
+                    takerlist.append(1)
+                else:
+                    takerlist.append(0)
+        else:
+            if int((firstwordarr[commonpointer]+secondwordarr[commonpointer])/2)>=14:
+                takerlist.append(1)
+            else:
+                takerlist.append(0)
+        commonpointer-=1
+    takerlist.reverse()
+    return takerlist
+    
+BackWardsWorker(Taker,FirstWordList,SecondWordList,StringLength)
+
+FinalWord = []
+
+def MedianTaker(stringlen, firstwordlist,secondwordlist,takerlist):
+    commonpointer = 0
+    initadder = 0
+    while commonpointer != stringlen:
+        if (firstwordlist[commonpointer]+secondwordlist[commonpointer])&1 == 0:
+            FinalWord.append(int(((firstwordlist[commonpointer]+secondwordlist[commonpointer])/2)+initadder))
+            initadder = 0
+        elif(firstwordlist[commonpointer]+secondwordlist[commonpointer])&1 == 1:
+            if takerlist[commonpointer+1]==1:
+                FinalWord.append(int(((firstwordlist[commonpointer]+secondwordlist[commonpointer])//2)+initadder+1))
+                initadder = -13
+            else:
+                FinalWord.append(int(((firstwordlist[commonpointer]+secondwordlist[commonpointer])//2)+initadder))
+                initadder = 13
+        commonpointer+=1
+    
+    return [chr(x+96) for x in FinalWord]
+
+tempstr = "".join(MedianTaker(StringLength,FirstWordList,SecondWordList,Taker))
+
+output(str(tempstr)+"\n")
