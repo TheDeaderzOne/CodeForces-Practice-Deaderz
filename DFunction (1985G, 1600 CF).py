905f1e58b80d557207046e54099805d5ec045fb0
TheDeaderzOne <137246950+TheDeaderzOne@users.noreply.github.com>
2025-07-15 23:29:41 -0400

    Add files via upload

---
+++ DFunction (1985G, 1600 CF).py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+numcases = int(input().replace("\n",""))
+
+for x in range(numcases):
+    l, r, k = [int(x) for x in input().replace("\n","").split()]
+    if k>=10: 
+        print(0)
+    else:
+        tempvar = 0
+        if 10 % k == 0: 
+            tempvar = 10//k
+        else:
+            tempvar = (10//k)+1
+        print((pow(tempvar, r, 10**9+7)- pow(tempvar,l,10**9+7) + 10**9+7)%(10**9+7))
+
+
+
